4c5d59d0482d3328fbe9cf2bde6162980852d068
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 20:37:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(a, b) {}
+function subtract(a,b) {}
+function multiply(a,b) {}
+function divide(a,b) {}
+function inc(n) {}
+function dec(n) {}
+function makeInt(n) {}
+function makeInt(n) {}
