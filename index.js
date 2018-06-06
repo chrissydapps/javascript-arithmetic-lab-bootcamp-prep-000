95523af1bb1dfd89fe4495393a2c20df88f4e4f6
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 20:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function multiply(a,b) {
 function divide(a,b) {
   return a/b
 }
-function inc(n) {}
-function dec(n) {}
-function makeInt(n) {}
-function makeInt(n) {}
+function inc(n) {
+  return n++
+}
+function dec(n) {
+  return n--
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
 function preserveDecimal(n) {}
 function preserveDecimal(n) {}
